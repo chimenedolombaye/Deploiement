50c8ee315b08328b8ee602090c779721abcbec4e
chimenedolombaye <chimenedolombaye@gmail.com>
2024-01-13 11:05:41 +0000

    Hello

--- Deploiement/app.py
+++ Deploiement/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 # Charger le modèle
 model = joblib.load('model.pkl')
 
+# Variables pour le suivi des transactions
+total_transactions = 0
+fraudulent_transactions_detected = 0
+
 # Définir un point de terminaison pour effectuer des prédictions
 @app.route('/')
 def home():
@@ -14,18 +18,27 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    global total_transactions, fraudulent_transactions_detected
+
     try:
         # Récupérer les données du formulaire
         V1 = float(request.form.get("V1"))
         V2 = float(request.form.get("V2"))
         Amount = float(request.form.get("Amount"))
 
-
         # Faire la prédiction avec le modèle
         prediction = model.predict([[V1, V2, Amount]])
 
-        # Retourner la prédiction au format JSON
-        return jsonify({"prediction": int(prediction[0])})
+        # Mettre à jour les variables de suivi
+        total_transactions += 1
+        if int(prediction[0]) == 1:  # Si la transaction est détectée comme frauduleuse
+            fraudulent_transactions_detected += 1
+
+        # Calculer le taux de fraude
+        fraud_rate = (fraudulent_transactions_detected / total_transactions) * 100
+
+        # Retourner la prédiction et le taux de fraude au format JSON
+        return jsonify({"prediction": int(prediction[0]), "fraud_rate": fraud_rate})
 
     except ValueError as e:
         return jsonify({"error": "Valeur de fonctionnalité manquante ou invalide"}), 400
